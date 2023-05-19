8597fc6cc480e01e81897619768561bcad3e2dd9
Muhammet Taha Demiryol <tahademiryol85@gmail.com>
2023-05-19 16:58:40 +0300

    change resilience4j retry settings

--- rental-service/src/main/java/com/tahademiryol/rentalservice/api/clients/CarClient.java
+++ rental-service/src/main/java/com/tahademiryol/rentalservice/api/clients/CarClient.java
@@ -1,6 +1,7 @@
 package com.tahademiryol.rentalservice.api.clients;
 
 import com.tahademiryol.commonpackage.utils.dto.ClientResponse;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +10,7 @@ import java.util.UUID;
 
 @FeignClient(name = "inventory-service", fallback = CarClientFallback.class)
 public interface CarClient {
+    @Retry(name = "retry-rental")
     @GetMapping("/api/cars/check-if-car-is-available/{carId}")
     ClientResponse checkIfCarIsAvailable(@PathVariable UUID carId);
 }
--- rental-service/src/main/java/com/tahademiryol/rentalservice/api/clients/CarClientFallback.java
+++ rental-service/src/main/java/com/tahademiryol/rentalservice/api/clients/CarClientFallback.java
@@ -1,7 +1,6 @@
 package com.tahademiryol.rentalservice.api.clients;
 
 import com.tahademiryol.commonpackage.utils.dto.ClientResponse;
-import io.github.resilience4j.retry.annotation.Retry;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
@@ -12,9 +11,11 @@ import java.util.UUID;
 public class CarClientFallback implements CarClient {
 
     @Override
-    @Retry(name = "retry-rental", fallbackMethod = "checkIfCarIsAvailable") // TODO! fallback check
+    // TODO! fallback check
     public ClientResponse checkIfCarIsAvailable(UUID carId) {
         log.info("INVENTORY IS DOWN!");
         throw new RuntimeException("INVENTORY-SERVICE IS NOT AVAILABLE RIGHT NOW!");
     }
+
+
 }
